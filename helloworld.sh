0dc9fb8e6f46cdba0bf05ac0f1672080d90d74c8
Mekalaabhiramreddy <abhiramr277@gmail.com>
2023-08-17 19:18:25 +0530

    first script

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#########################################################################
+# Author: abhiram reddy
+# Version: v1.0.0
+# Date: 17-08-2023
+# Description: This is first script 
+# Usage: ./helloworld.sh
+#########################################################################
+
+echo "hello world"
